669cbd8131b0585867c0dd84869b85dbae9239ae
dilukshashamal <dilukshashamal2001@gmail.com>
2024-09-18 22:29:28 +0530

    update unit test

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 import pytest
 import pandas as pd
 from src.pipeline.predict_pipeline import PredicPipeline, CustomData
